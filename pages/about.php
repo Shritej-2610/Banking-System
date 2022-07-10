71c96c9d2e05daa061185dcd8f9e13f9e8793121
Shritej <shritejtodankar2001@gmail.com>
2022-07-10 19:29:54 +0530

    first commit

---
+++ pages/about.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php $title = "About";
+$active = "active";
+include "header.php" ?>
+
+<section id="services" class="services section-bg">
+    <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+            <h2>About Team</h2>
+            <p>Teamwork is the ability to work together toward a common vision.</p>
+        </div>
+
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+                <div class="icon-box iconbox-green">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHSzafkSKO88A/profile-displayphoto-shrink_800_800/0/1628708373951?e=1663200000&v=beta&t=SeOnamzoAuld26AUEArc2tm1mnHDzAEIS4i3UccKgcE" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Digvijay Phutane</a></h4>
+                    <p class="mt-1"> <strong>(Designer & Developer)</strong></p>
+                    <p class="mt-3">The Man Who Has Power of Imagination, He Has a Wings</p>
+
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon-box iconbox-blue ">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFivjJoJ-xqVA/profile-displayphoto-shrink_800_800/0/1622277830251?e=1663200000&v=beta&t=4aVGBMmfURNbmoSVjHSYModJHH1ZFO6G_g5tFH6xpg4" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Hrushikesh Gaware</a></h4>
+                    <p class="mt-1"> <strong>(Designer & Developer)</strong></p>
+                    <p class="mt-3">Everything should be made as simple as possible, but not simpler.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+                <div class="icon-box iconbox-pink">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFlvNN_1Rrupg/profile-displayphoto-shrink_800_800/0/1630746995490?e=1663200000&v=beta&t=RgZVBxio4yuRLXN-_aJzb5m5bZjvvYMVnkvDxrHjyL8" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Sheetal Munde</a></h4>
+                    <p class="mt-1"> <strong>(Frontend Developer)</strong></p>
+                    <p class="mt-3">Beauty in things exists in the mind which contemplates them.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="100">
+
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="200">
+                <div class="icon-box iconbox-pink">
+                    <div class="icon" style="width: 160px; height: 160px;">
+                        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQGxdfPEb6hL6g/profile-displayphoto-shrink_800_800/0/1655831023919?e=1663200000&v=beta&t=0rl94T6kpmb6xOCOKp1J5TnN-2vVqkwRlJpyyANO1hw" class="img-round" alt="">
+                    </div>
+                    <h4><a href="">Shritej Todankar</a></h4>
+                    <p class="mt-1"> <strong>(Software Tester)</strong></p>
+                    <p class="mt-3">The principle objective of software testing is to give confidence in the
+                        software.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="zoom-in" data-aos-delay="300">
+
+            </div>
+
+        </div>
+
+    </div>
+</section>
+
+<?php
+include "footer.php";
+?>
+
+
+</html>
